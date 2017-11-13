c5bce6b32b7ad206e874f8361ad4bc94f7af5856
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-11-13 16:42:03 +0900

    ノードをダイナミックに変える。

---
+++ Assets/Scripts/AStar/DynamicNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YYGAStar
+{
+	public class DynamicNode : MonoBehaviour
+	{
+
+		public Grid grid;
+
+		void Update ()
+		{
+			if (Input.GetMouseButton (1)) {
+				if (grid == null)
+					return;
+				RaycastHit hit;
+				if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, Mathf.Infinity, 1 << Grid.groundLayer)) {
+					Vector3 pos = hit.point;
+					Node node = grid.GetNode (pos);
+					if (!node.isBlock) {
+						node.isBlock = true;
+						for (int i = 0; i < node.neighbors.Count; i++) {
+							grid.CalculateNeighbor (node.neighbors[i]);
+						}
+					}
+				}
+			}
+		}
+
+	}
+}
