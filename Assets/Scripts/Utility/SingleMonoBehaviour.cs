2940ff98f8a25c857bf27b198f944e78dc783b22
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-11-15 17:45:30 +0900

    ツールクラス。

---
+++ Assets/Scripts/Utility/SingleMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour{
+
+	private static T t;
+
+	public static T GetInstance{
+		get{
+			if (t == null) {
+				t = GameObject.FindObjectOfType (typeof(T)) as T;
+				if (t == null) {
+					GameObject go = new GameObject ();
+					t = go.AddComponent<T> ();
+				}
+			}
+			return t;
+		}
+	}
+
+	protected virtual void Awake(){
+		if(t==null){
+			t = gameObject.GetComponent<T> ();
+		}
+	}
+}
