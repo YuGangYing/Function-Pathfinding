d9e0ff2fed466fb83f5030ecf901bd7cd49771fd
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-11-16 14:55:06 +0900

    グルプで捜索する方法を追加している。（臨時、調整が必要だ）

--- Assets/Scripts/AStar/RVO/RVOController.cs
+++ Assets/Scripts/AStar/RVO/RVOController.cs
@@ -6,11 +6,25 @@ namespace YYGAStar
 {
 	public class RVOController : SingleMonoBehaviour<RVOController>
 	{
-
+		public Grid grid;
 		public Dictionary<int,RVOGroup> agentGroup;
-
+		public bool rvo = true;
 		protected override void Awake ()
 		{
+			InitGroup ();
+		}
+
+		void Update ()
+		{
+			if (Input.GetMouseButtonDown (0)) {
+				if(rvo)
+					RVOMove (0);
+				else
+					NormalMove(0);
+			}
+		}
+
+		void InitGroup(){
 			agentGroup = new Dictionary<int, RVOGroup> ();
 			PathAgent[] agents = FindObjectsOfType<PathAgent> (); 
 			for(int i=0;i<agents.Length;i++){
@@ -26,31 +40,81 @@ namespace YYGAStar
 			}
 		}
 
-		void Update ()
-		{
-			if (Input.GetMouseButtonDown (0)) {
-				RVOGroup group0 = agentGroup [0];
-				PathAgent leader = group0.leader;
-				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
-				RaycastHit hit;
-				if (Physics.Raycast (ray, out hit, Mathf.Infinity, 1 << Grid.groundLayer)) {
-					Vector3 hitPos = hit.point;
-//					List<Node> smoothPath = leader.StartFinder (hitPos);
-//					MoveAgent moveAgent = leader.GetComponent<MoveAgent> ();
-//					if (moveAgent != null)
-//						moveAgent.Move (smoothPath);
-
-//					for (int i = 0; i < group0.members.Count; i++) {
-//						List<Node> smoothPathMember = group0.members[i].StartFinder (leader.transform.position);
-//						smoothPathMember.AddRange (smoothPath);
-//						MoveAgent moveAgentMember = group0.members[i].GetComponent<MoveAgent> ();
-//						if (moveAgentMember != null)
-//							moveAgentMember.Move (smoothPathMember);
-//					}
+		void RVOMove(int groupId){
+			RVOGroup group0 = agentGroup [groupId];
+			PathAgent leader = group0.leader;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity, 1 << Grid.groundLayer)) {
+				float time = Time.realtimeSinceStartup;
+				Node targetNode = grid.GetNode (hit.point);
+				Node leaderNode = grid.GetNode (leader.transform.position);
+				List<Node> smoothPath = leader.StartFinder (leaderNode,targetNode);
+				MoveAgent moveAgent = leader.GetComponent<MoveAgent> ();
+				if (moveAgent != null)
+					moveAgent.Move (smoothPath);
+				List <Node> nearest = grid.GetNearest (targetNode,group0.members.Count);
+				for (int i = 0; i < group0.members.Count; i++) {
+					List<Node> smoothPathMember = group0.members[i].StartFinder (grid.GetNode (group0.members[i].transform.position),leaderNode);
+					smoothPathMember.AddRange (smoothPath);
+					List<Node> endPath = group0.members [i].StartFinder (smoothPathMember [smoothPathMember.Count - 2], nearest[i]);
+					smoothPathMember.RemoveAt (smoothPathMember.Count-1);
+					smoothPathMember.AddRange (endPath);
+					MoveAgent moveAgentMember = group0.members[i].GetComponent<MoveAgent> ();
+					if (moveAgentMember != null)
+						moveAgentMember.Move (smoothPathMember);
 				}
+				Debug.Log ("Total Search Time:" + (Time.realtimeSinceStartup - time));
 			}
 		}
 
+		void NormalMove(int groupId){
+			RVOGroup group0 = agentGroup [groupId];
+			PathAgent leader = group0.leader;
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit, Mathf.Infinity, 1 << Grid.groundLayer)) {
+				float time = Time.realtimeSinceStartup;
+				Node targetNode = grid.GetNode (hit.point);
+				Node leaderNode = grid.GetNode (leader.transform.position);
+				List<Node> smoothPath = leader.StartFinder (leaderNode,targetNode);
+				MoveAgent moveAgent = leader.GetComponent<MoveAgent> ();
+				if (moveAgent != null)
+					moveAgent.Move (smoothPath);
+				List <Node> nearest = grid.GetNearest (targetNode,group0.members.Count);
+				for (int i = 0; i < group0.members.Count; i++) {
+					List<Node> smoothPathMember = group0.members[i].StartFinder (grid.GetNode (group0.members[i].transform.position),nearest[i]);
+//					smoothPathMember.AddRange (smoothPath);
+//					List<Node> endPath = group0.members [i].StartFinder (smoothPathMember [smoothPathMember.Count - 2], nearest[i]);
+//					smoothPathMember.RemoveAt (smoothPathMember.Count-1);
+//					smoothPathMember.AddRange (endPath);
+					MoveAgent moveAgentMember = group0.members[i].GetComponent<MoveAgent> ();
+					if (moveAgentMember != null)
+						moveAgentMember.Move (smoothPathMember);
+				}
+				Debug.Log ("Total Search Time:" + (Time.realtimeSinceStartup - time));
+			}
+		}
+
+
+//		List <Node> nearest = new List<Node>();
+//		void GetNearest(){
+//			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+//			RaycastHit hit;
+//			if (Physics.Raycast (ray, out hit, Mathf.Infinity, 1 << Grid.groundLayer)) {
+//				Node node = grid.GetNode (hit.point);
+//				RVOGroup group0 = agentGroup [0];
+//				nearest = grid.GetNearest (node,group0.members.Count);
+//			}
+//		}
+//
+//		void OnDrawGizmos ()
+//		{
+//			Gizmos.color = Color.yellow;
+//			for(int i=0;i<nearest.Count;i++){
+//				Gizmos.DrawCube (nearest[i].pos, Vector3.one * 1.4f);
+//			}
+//		}
 	}
 
 	public class RVOGroup{
