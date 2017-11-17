f1f7eb43b8edadb031895cbbcddbd707b8fdefb8
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-11-17 17:24:40 +0900

    パラメーター「speed」を「movePerFrame」に変更した。

--- Assets/Scripts/AStar/MoveAgent.cs
+++ Assets/Scripts/AStar/MoveAgent.cs
@@ -5,10 +5,9 @@ using YYGAStar;
 
 public class MoveAgent : MonoBehaviour {
 
-	public float speed = 0.1f;
+	public float movePerFrame = 0.8f;
 	public List<Node> path;
 	public bool movable = true;
-
 	int mCurrentIndex = 0;
 	Transform mTrans;
 
@@ -16,25 +15,25 @@ public class MoveAgent : MonoBehaviour {
 		mTrans = transform;
 	}
 
-	//以帧的方式移动。
+	//時間を使用しない。
 	void Update(){
 		if(path!=null && path.Count > mCurrentIndex && movable){
 			float moved = 0;
 			Vector3 startPos = mTrans.position;
-			while(moved < speed){
+			while(moved < movePerFrame){
 				if (path.Count ==  mCurrentIndex) {
 					break;
 				}
 				Node node = path[mCurrentIndex];
 				mTrans.LookAt (node.pos);
 				float dis = Vector3.Distance (mTrans.position,node.pos);
-				if (speed - moved > dis) {
+				if (movePerFrame - moved > dis) {
 					moved += dis;
 					mCurrentIndex++;
 					mTrans.position = node.pos;
 				} else {
-					mTrans.position += mTrans.forward * (speed - moved);
-					moved = speed;
+					mTrans.position += mTrans.forward * (movePerFrame - moved);
+					moved = movePerFrame;
 				}
 			}
 		}
